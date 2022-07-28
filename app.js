8481e95119b304e6b2ca9e643b983048f4ae3abc
Joao Pinto <joaomdiaspinto@gmail.com>
2022-07-28 16:14:56 +0100

    Computer makes a move and the game ends showing who wins the game.

--- app.js
+++ app.js
@@ -5,9 +5,64 @@ const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll(".game-options");
 
 let userChoice;
+let computerChoice;
+let result;
 
 possibleChoices.forEach(choice => 
         choice.addEventListener('click', (e) => {
             userChoice=e.target.id;
             userChoiceDisplay.innerHTML = userChoice;
+            generateComputerChoice();
+            getResult();
         }));
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)+1;
+
+    switch(randomNumber){
+        case 1:
+            computerChoice = 'rock';
+            break;
+        case 2:
+            computerChoice = 'scissor';
+            break;
+        case 3:
+            computerChoice = 'paper';
+            break;
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult(){
+    
+    if(userChoice === computerChoice){
+        result = 'its a draw';
+    }
+    
+    else if (computerChoice === 'rock'){
+        if(userChoice === 'paper'){
+            result = 'you win!';
+        } else {
+            result = 'you loose!';
+        }
+    }
+
+    else if (computerChoice === 'paper'){
+        if(userChoice === 'scissor'){
+            result = 'you win!';
+        } else {
+            result = 'you loose!';
+        }  
+    }
+
+    else if (computerChoice === 'scissor'){
+        if(userChoice === 'rock'){
+            result = 'you win!';
+        } else {
+            result = 'you loose!';
+        }
+    }
+
+    resultDisplay.innerHTML = result;
+}
