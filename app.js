c5505ebb7190dacf64350904d637f04f6587c1d5
Joao Pinto <joaomdiaspinto@gmail.com>
2022-07-28 15:38:08 +0100

    user can make choice the option

---
+++ app.js
@@ -0,0 +1,14 @@
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+
+const possibleChoices = document.querySelectorAll(".game-options");
+
+let userChoice;
+
+possibleChoices.forEach(choice => 
+        choice.addEventListener('click', (e) => {
+            userChoice=e.target.id;
+            console.log(userChoice);
+            userChoiceDisplay.innerHTML = userChoice;
+        }));
